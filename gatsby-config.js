7439ba2a5e61570d07b38fbd3798690f0a24d982
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-09-24 00:20:55 -0400

    clean files and use css-in-module

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	pathPrefix: '/yonghe-online',
 	siteMetadata: {
-		title: 'Gatsby Default Starter',
-		description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-		author: '@gatsbyjs',
+		title: 'title',
+		description: 'description',
+		author: 'author',
 	},
 	plugins: [
 		'gatsby-plugin-react-helmet',
@@ -16,18 +16,8 @@ module.exports = {
 		},
 		'gatsby-transformer-sharp',
 		'gatsby-plugin-sharp',
-		{
-			resolve: 'gatsby-plugin-manifest',
-			options: {
-				name: 'gatsby-starter-default',
-				short_name: 'starter',
-				start_url: '/',
-				background_color: '#663399',
-				theme_color: '#663399',
-				display: 'minimal-ui',
-				icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-			},
-		},
+		'gatsby-plugin-sass',
+		'gatsby-plugin-manifest'
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
