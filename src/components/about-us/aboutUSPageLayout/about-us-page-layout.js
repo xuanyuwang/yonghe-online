4d32b9690af1f51331924a21841d27d1681c3a71
Xuanyu Wang <Xuanyu.Wang@ibm.com>
2020-10-02 22:58:24 -0400

    add top

--- src/components/about-us/aboutUSPageLayout/about-us-page-layout.js
+++ src/components/about-us/aboutUSPageLayout/about-us-page-layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutUSMiddle from '../about-us-middle/about-us-middle';
-import './about-us-page-layout.scss';
 import OurTeam from '../about-us-ourteam/ourTeam';
+import AboutUSTop from '../about-us-top/about-us-top';
+import './about-us-page-layout.scss';
 class AboutUSPageLayout extends React.Component {
 	render() {
 		return (
 			<div>
+				<AboutUSTop></AboutUSTop>
 				<AboutUSMiddle></AboutUSMiddle>
 				<OurTeam />
 			</div>
